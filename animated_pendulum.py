8aaba669470bce9d7e15c90bb44df347e94edb4a
simamzadeh <sima.mz246@gmail.com>
2021-03-02 16:08:58 +0000

    Set constants and make initial values
    set initial length and amplitude, plot line using x and y values (with limits set for graph)

--- animated_pendulum.py
+++ animated_pendulum.py
@@ -4,3 +4,23 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
 
+#Set constants
+length = 5
+initial_amp = 150
+
+#Make initial values for line
+# x_vals = np.linspace(7.5, 10, length)
+# y_vals = np.ones(length)*5.5
+x_vals = [7.5, 10]
+y_vals = [5.5, 10]
+
+
+#Plotting the graph
+plt.plot(x_vals, y_vals)
+
+#Adding limits to graph
+plt.xlim(5, 15)
+plt.ylim(4, 11)
+
+plt.plot(x_vals, y_vals)
+plt.show()
