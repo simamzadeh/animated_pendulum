51e65658126c0560e058aa0e1f7806d158d0c375
simamzadeh <sima.mz246@gmail.com>
2021-03-02 18:58:32 +0000

    Update method for plotting line
    Instead, use trigonometry to find the x-length and y-height of the the line (taking the line of length 5 to be the hypotenuse). Find gradient, then calculate x and y coordinates to plot.

--- animated_pendulum.py
+++ animated_pendulum.py
@@ -1,22 +1,47 @@
-#Import modules
+# Import modules
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
 
-#Set constants
+# Set constants
 length = 5
 initial_amp = 150
+g = 9.8
 
-#Make initial values for line
-# x_vals = np.linspace(7.5, 10, length)
-# y_vals = np.ones(length)*5.5
-x_vals = [7.5, 10]
-y_vals = [5.5, 10]
+
+# Plot pendulum at 30 degrees
+
+x_length = np.sin(30*(np.pi/180))*5
+y_length = np.cos(30*(np.pi/180))*5
+gradient = y_length/x_length
+
+x_coord = 10 - x_length
+y_coord = 10 - y_length
+
+x_vals = [x_coord, 10]
+y_vals = [y_coord, 10]
+
+
+# timepoints = np.arange(x5, 15, 2)
+# for t in timepoints:
+#     displacement = 150*np.cos(np.sqrt(9.8/5))
+#
+#
+# def time(angle):
+#     x_vals = np.linspace(7.5, 10, data_points)
+#     y_vals = (np.sqrt(g/length)) * (1/angle)
+#     return x_vals, y_vals
+#
+#
+# print(time(30))
+
+# def length(angle):
+#     return (9.8/(angle)**2) * 1/timepoints
 
 
 #Plotting the graph
-plt.plot(x_vals, y_vals)
+# plt.plot(x_vals, y_vals)
 
 #Adding limits to graph
 plt.xlim(5, 15)
