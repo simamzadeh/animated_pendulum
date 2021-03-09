2aabf9e627ca040e0368188a4e6ec5ba0033b7c7
simamzadeh <sima.mz246@gmail.com>
2021-03-09 19:16:02 +0000

    Update length
    Update length of second pendulum and I am finished!

--- animated_pendulum.py
+++ animated_pendulum.py
@@ -5,7 +5,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
 
 # Set constants
-length = 5
+length1 = 5
 amplitude = 150
 g = 9.8
 length2 = 3
@@ -37,7 +37,7 @@ y_vals2 = [y_coord2, 10]
 
 # Defining a function to take the angle and return the x and y coordinates
 
-def find_coordinates(angle):
+def find_coordinates(angle, length):
     x_length = np.sin(angle * (np.pi / 180)) * length
     y_length = np.cos(angle * (np.pi / 180)) * length
 
@@ -62,21 +62,22 @@ for t in timepoints:
 
     # Reducing the amplitude by 5% each time
     amplitude = amplitude * 0.95
-    displacement = amplitude * np.cos(np.sqrt(g / length) * t)
+    displacement1 = amplitude * np.cos(np.sqrt(g / length1) * t)
+    displacement2 = amplitude * np.cos(np.sqrt(g / length2) * t)
 
-    angle1 = displacement / length
+    angle1 = displacement1 / length1
     angles1.append(angle1)
 
     # PENDULUM 2
-    angle2 = displacement / length2
+    angle2 = displacement2 / length2
     angles2.append(angle2)
 
 # Plot the pendulum and endpoint
-x_vals, y_vals, x_coord, y_coord = find_coordinates(angle1)
+x_vals, y_vals, x_coord, y_coord = find_coordinates(angle1, length1)
 pendulum1, = plt.plot(x_vals, y_vals)
 endpoint1, = plt.plot(x_coord, y_coord, color="g", marker=".")
 
-x_vals, y_vals, x_coord, y_coord = find_coordinates(angle2)
+x_vals, y_vals, x_coord, y_coord = find_coordinates(angle2, length2)
 pendulum2, = plt.plot(x_vals, y_vals)
 endpoint2, = plt.plot(x_coord, y_coord, color="r", marker=".")
 
@@ -88,8 +89,8 @@ def animate_pendulum(frame, local_angles1, local_angles2):
     current_angle1 = local_angles1[frame]
     current_angle2 = local_angles2[frame]
 
-    x_vals1, y_vals1, x_coord1, y_coord1 = find_coordinates(current_angle1)
-    x_vals2, y_vals2, x_coord2, y_coord2 = find_coordinates(current_angle2)
+    x_vals1, y_vals1, x_coord1, y_coord1 = find_coordinates(current_angle1, length1)
+    x_vals2, y_vals2, x_coord2, y_coord2 = find_coordinates(current_angle2, length2)
 
     pendulum1.set_data(x_vals1, y_vals1)
     pendulum2.set_data(x_vals2, y_vals2)
@@ -104,7 +105,6 @@ plt.ylim(4, 11)
 
 # Calling the animation functions
 pendulum_anim = FuncAnimation(plt.gcf(), animate_pendulum, frames=frames, repeat=True, fargs= [angles1, angles2])
-
 
 
 # Plotting the centre and end of the pendulum
